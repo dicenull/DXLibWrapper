e61b389b45398c5e48f820a2d664ee5fb50bc74d
dice <dicenull.dev@gmail.com>
2018-07-01 15:29:13 +0900

    DXライブラリに関するプロジェクトを追加

---
+++ DxLibUtilities/SceneManager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DxLibUtilities
+{
+    public class SceneManager
+    {
+        private IScene currentScene;
+        
+        public SceneManager(IScene scene)
+        {
+            currentScene = scene;
+        }
+
+        public bool Update()
+        {
+            var nextScene = currentScene.Update();
+
+            if(nextScene == null)
+            {
+                return false;
+            }
+
+            currentScene = nextScene;
+            return true;
+        }
+    }
+}
