fe0bfa57f2e64ca7c0460eaad424d168ada349e4
dice <dicenull.dev@gmail.com>
2018-07-01 15:28:23 +0900

    DXライブラリとシーンを追加

---
+++ DiceVsYosanoReMake/MainScene.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DxLibUtilities;
+using DxLibDLL;
+
+namespace DiceVsYosanoReMake
+{
+    class MainScene : IScene
+    {
+        public void Draw()
+        {
+        }
+
+        public IScene Update()
+        {
+            return this;
+        }
+    }
+}
--- DiceVsYosanoReMake/Program.cs
+++ DiceVsYosanoReMake/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DxLibDLL;
+using DxLibUtilities;
 
 namespace DiceVsYosanoReMake
 {
@@ -10,6 +12,21 @@ namespace DiceVsYosanoReMake
     {
         static void Main(string[] args)
         {
+            DX.DxLib_Init();
+            DX.SetDrawScreen(DX.DX_SCREEN_BACK);
+
+            SceneManager sceneManager = new SceneManager(new MainScene());
+            
+            while(DX.ProcessMessage() != -1)
+            {
+                DX.ClearDrawScreen();
+                
+
+
+                DX.ScreenFlip();
+            }
+
+            DX.DxLib_End();
         }
     }
 }
