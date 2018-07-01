3c91a002ea3d447ea79e4f50857ea2a8abb89c93
dice <dicenull.dev@gmail.com>
2018-07-01 15:28:47 +0900

    座標や図形に関するプロジェクトを追加

---
+++ Diagram/Point.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Diagram
+{
+    public struct Point<T> where T : struct
+    {
+        public static readonly Point<int> Zero = new Point<int>(0, 0);
+
+        public T X { get; set; }
+        public T Y { get; set; }
+        
+        public Point(T x, T y)
+        {
+            X = x;
+            Y = y;
+        }
+        
+        public Point(Point<T> size)
+        {
+            X = size.X;
+            Y = size.Y;
+        }
+        
+    }
+}
---
+++ Diagram/Rect.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DxLibUtilities
+{
+    public class Rect
+    {
+        public int X { get; private set; }
+        public int Y { get; private set; }
+    }
+}
