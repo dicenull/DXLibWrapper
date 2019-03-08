04381fc0aa98494a472a5048af5a0806b463ccb8
dice <dicenull.dev@gmail.com>
2019-03-09 08:40:10 +0900

    ColorクラスとPaletteクラスを追加

---
+++ Diagram/Palette.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Diagram
+{
+    public class Palette
+    {
+        public static readonly Color White = new Color(255, 255, 255);
+    }
+}
