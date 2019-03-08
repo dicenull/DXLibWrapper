024d1c2ab3f7503fde96e24584353f36319ddf07
s1500729 <s1500729@sendai-nct.jp>
2019-03-08 22:43:48 +0900

    四角形の描画部分を追加

---
+++ Diagram/Rectangle_DX.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DxLibDLL;
+
+namespace Diagram
+{
+    /// <summary>
+    /// 四角形クラス描画部
+    /// </summary>
+    public partial class Rectangle
+    {
+        public void Draw()
+        {
+            int x = Point.X, y = Point.Y;
+            int w = Size.X, h = Size.Y;
+
+            // TODO : 色を指定できるように
+            DX.DrawBox(x, y, x + w, y + h, DX.GetColor(255, 255, 255), 1);
+        }
+    }
+}
