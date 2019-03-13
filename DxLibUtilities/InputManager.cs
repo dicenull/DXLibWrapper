4b22d10995307eee953c28bbb4282b8b5437b84e
s1500729 <s1500729@sendai-nct.jp>
2019-03-14 01:11:24 +0900

    マウスとキー入力を管理するクラスを追加

---
+++ DxLibUtilities/InputManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DxLibUtilities
+{
+    public static class InputManager
+    {
+        public static Key Key { get; } = new Key();
+
+        public static Mouse Mouse { get; } = new Mouse();
+
+        public static void Update()
+        {
+            Key.Update();
+            Mouse.Update();
+        }
+    }
+}
