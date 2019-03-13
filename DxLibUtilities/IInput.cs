8f6c63ecc5874ccd50e0c913a1f52265d43bc389
s1500729 <s1500729@sendai-nct.jp>
2019-03-14 00:29:55 +0900

    入力用インターフェース、キー入力を管理するクラスを追加

---
+++ DxLibUtilities/IInput.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DxLibUtilities
+{
+    interface IInput
+    {
+        void Update();
+
+        /// <summary>
+        /// 押され始めたか
+        /// </summary>
+        bool IsDown(int code);
+
+        /// <summary>
+        /// 押されているか
+        /// </summary>
+        bool IsPressed(int code);
+
+        /// <summary>
+        /// 離され始めたか
+        /// </summary>
+        bool IsRelease(int code);
+
+        /// <summary>
+        /// 離されているか
+        /// </summary>
+        bool IsUp(int code);
+    }
+}
---
+++ DxLibUtilities/Key.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DxLibDLL;
+
+namespace DxLibUtilities
+{
+    public class Key : IInput
+    {
+        public Key()
+        {
+            Update();
+        }
+
+        public bool IsDown(int code)
+        {
+            return keys[code][prev] == 0 && keys[code][flip] != 0;
+        }
+
+        public bool IsPressed(int code)
+        {
+            return keys[code][flip] != 0;
+        }
+
+        public bool IsRelease(int code)
+        {
+            return keys[code][flip] == 0;
+        }
+
+        public bool IsUp(int code)
+        {
+            return keys[code][prev] != 0 && keys[code][flip] == 0;
+        }
+
+        public void Update()
+        {
+            flip = 1 - flip;
+            
+            DX.GetHitKeyStateAll(keys[flip]);
+        }
+
+        private byte[][] keys = { new byte[256], new byte[256] };
+        private int flip = 0;
+        private int prev { get { return 1 - flip; } }
+    }
+}
