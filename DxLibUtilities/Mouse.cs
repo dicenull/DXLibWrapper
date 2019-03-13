20e690894a781a21b463ac57a8febc65546c8744
s1500729 <s1500729@sendai-nct.jp>
2019-03-14 01:11:06 +0900

    マウス入力を管理するクラスを追加

---
+++ DxLibUtilities/Mouse.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using DxLibDLL;
+
+namespace DxLibUtilities
+{
+    public class Mouse : IInput<MouseButton>
+    {
+        public Mouse()
+        {
+            Update();
+        }
+
+        public bool IsDown(MouseButton button)
+        {
+            return (mouseState[prev] & button.ToCode()) == 0
+                && (mouseState[flip] & button.ToCode()) != 0;
+        }
+
+        public bool IsPressed(MouseButton button)
+        {
+            return (mouseState[flip] & button.ToCode()) != 0;
+        }
+
+        public bool IsRelease(MouseButton button)
+        {
+            return (mouseState[flip] & button.ToCode()) == 0;
+        }
+
+        public bool IsUp(MouseButton button)
+        {
+            return (mouseState[prev] & button.ToCode()) != 0
+                && (mouseState[flip] & button.ToCode()) == 0;
+        }
+
+        public void Update()
+        {
+            flip = 1 - flip;
+
+            mouseState[flip] = DX.GetMouseInput();
+        }
+
+        private int flip = 0;
+        private int prev { get { return 1 - flip; } }
+        private int[] mouseState = { 0, 0 };
+    }
+}
---
+++ DxLibUtilities/MouseButton.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using DxLibDLL;
+
+namespace DxLibUtilities
+{
+    public enum MouseButton
+    {
+        Left,
+        Right,
+        Middle,
+        Button4,
+        Button5,
+        Button6,
+        Button7,
+        Button8
+    }
+
+    public static class MouseButtonExtentions
+    {
+        public static int ToCode(this MouseButton button)
+        {
+            return button switch
+            {
+                MouseButton.Left => DX.MOUSE_INPUT_LEFT,
+                MouseButton.Right => DX.MOUSE_INPUT_RIGHT,
+                MouseButton.Middle => DX.MOUSE_INPUT_MIDDLE,
+                MouseButton.Button4 => DX.MOUSE_INPUT_4,
+                MouseButton.Button5 => DX.MOUSE_INPUT_5,
+                MouseButton.Button6 => DX.MOUSE_INPUT_6,
+                MouseButton.Button7 => DX.MOUSE_INPUT_7,
+                MouseButton.Button8 => DX.MOUSE_INPUT_8,
+                _ => throw new ArgumentException()
+            };
+        }
+    }
+}
