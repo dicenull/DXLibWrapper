5e624729904087aac5ed024b68ecab87aaea2cf1
s1500729 <s1500729@sendai-nct.jp>
2019-03-16 17:45:42 +0900

    Textクラスの描画部を作成

---
+++ DxLibUtilities/Text.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using DxLibDLL;
+using DxLibUtilities;
+using Utilities;
+
+namespace DxLibUtilities
+{
+    public class Text
+    {
+        private int handle;
+
+        public Text(int size)
+        {
+            handle = DX.CreateFontToHandle(null, size, -1);
+        }
+
+        public void Draw(string text)
+        {
+            Draw(text, Vector2D.GetZero, Palette.White);
+        }
+        
+        public void Draw(string text, Vector2D pos, Color color)
+        {
+            DX.DrawStringToHandle(pos.X, pos.Y, text, color.ToDxColor(), handle);
+        }
+    }
+}
--- Utilities/Vector2D.cs
+++ Utilities/Vector2D.cs
@@ -1,8 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using Utilities;
 
-namespace Diagram
+namespace Utilities
 {
     public struct Vector2D
     {
