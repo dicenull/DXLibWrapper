8426655ecee80c6c2487d220a9eb2fbb917ca55b
s1500729 <s1500729@sendai-nct.jp>
2019-03-18 09:26:41 +0900

    ランダム値を取得するクラスを追加

---
+++ DxLibUtilities/Random.cs
@@ -0,0 +1,28 @@
+﻿using DxLibDLL;
+
+namespace DxLibUtilities
+{
+    public static class Random
+    {
+        /// <returns>0からint最大値までの乱数</returns>
+        public static int Next()
+        {
+            return DX.GetRand(int.MaxValue);
+        }
+
+        /// <param name="maxValue">乱数の最大値</param>
+        /// <returns>0から指定した値までの乱数</returns>
+        public static int Next(int maxValue)
+        {
+            return DX.GetRand(maxValue);
+        }
+
+        /// <param name="minValue">乱数の最小値</param>
+        /// <param name="maxValue">乱数の最大値</param>
+        /// <returns>指定した範囲の乱数</returns>
+        public static int Next(int minValue, int maxValue)
+        {
+            return DX.GetRand(maxValue - minValue) + minValue;
+        }
+    }
+}
